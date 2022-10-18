d1414f20f50ff2aa77ada72cb8920d171f5e0a8b
Tatev <111972060+tatevbezhanyan@users.noreply.github.com>
2022-10-18 04:52:44 +0400

    Add files via upload

---
+++ server4.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var fs = require("fs");
+
+function onRequest(request, response){
+    response.writeHead(200,{"Content-Type": "text/html"});
+    fs.readFile("button.html", null, function(error, data){
+        if (error){
+            response.writeHead(404);
+            response.write("File not found!");
+        } else {
+            console.log(data);
+            response.write(data);
+        }
+        response.end();
+    })
+}
+
+http.createServer(onRequest).listen(8080);
